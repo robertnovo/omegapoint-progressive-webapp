5dc9c6ae0a43cfe1a1c9299ae5f5b8d89c8ecc21
Robert Novo <robert.j.novo@gmail.com>
2016-12-06 22:10:52 +0100

    Changed port name

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/images', (req, res) => {
 
 });
 
-const PORT = process.env.port || 4444;
+const PORT = process.env.PORT || 4444;
 app.listen(PORT, () => {
   console.log('Server running on', PORT);
 })
